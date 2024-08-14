886fb3cbf3d6171e58e37e42475623543e5b77cd
omar-t-ahmed <omartahmed@gmail.com>
2024-08-14 19:56:12 -0400

    add styling

--- app/page.js
+++ app/page.js
@@ -1,10 +1,13 @@
+import MaxWidthWrapper from "@/components/MaxWidthWrapper";
 
 export default function Home() {
   return (
-    <main>
-      <div>
-        Flashcard App
-      </div>
+    <main className="bg-zinc-900 min-h-screen text-white">
+      <MaxWidthWrapper className="py-4">
+        <div>
+          Flashcard App
+        </div>
+      </MaxWidthWrapper>
     </main>
   );
 }
